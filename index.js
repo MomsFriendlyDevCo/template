a6331545a918e702c2fa41f48a52fb4f98b7bdec
Matt Carter <m@ttcarter.com>
2019-06-11 12:42:17 +1000

    BUGFIX: Typo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ templator.compile = (template, options) => {
 	var settings = {...templator.defaults, ...options};
 	if (settings.handlebars) template = template.replace(/{{(.+?)}}/g, '\${$1}');
 	if (settings.dotted) { // Tidy up dotted notation
-		template = template.replace(/\${\s*([a-z0-9\._]+?)\s*}/g, (match, expr) =>
+		template = template.replace(/\${\s*([a-z0-9\._]+?)\s*}/ig, (match, expr) =>
 			'${'
 			+ expr
 				.replace(/\.([0-9]+)/g, '[$1]') // Rewrite '.123' -> '[123]'
