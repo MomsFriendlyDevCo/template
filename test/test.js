7fa96c4c4bf64b08abbcf322ec06518e4cbfb5c0
Matt Carter <m@ttcarter.com>
2023-02-16 14:14:53 +1000

    FEAT: Added Browser compliant version

--- test/test.js
+++ test/test.js
@@ -1,51 +1,54 @@
-var expect = require('chai').expect;
-var template = require('..');
+const expect = require('chai').expect;
 
-describe('template()', ()=> {
+['browser', 'node'].forEach(method => {
+	let template = require(`../lib/${method}`);
 
-	it('should render simple variable replacements', ()=> {
-		var data = {
-			aNumber: 123,
-			aDate: new Date('1990-01-01'),
-			aString: 'Hello World',
-		};
-		expect(template('Number: ${aNumber}', data)).to.equal(`Number: ${data.aNumber}`);
-		expect(template('Date: ${aDate}', data)).to.equal(`Date: ${data.aDate}`);
-		expect(template('String: ${aString}', data)).to.equal(`String: ${data.aString}`);
+	describe(`${method.toUpperCase()}: template()`, ()=> {
+
+		it('should render simple variable replacements', ()=> {
+			let data = {
+				aNumber: 123,
+				aDate: new Date('1990-01-01'),
+				aString: 'Hello World',
+			};
+			expect(template('Number: ${aNumber}', data)).to.equal(`Number: ${data.aNumber}`);
+			expect(template('Date: ${aDate}', data)).to.equal(`Date: ${data.aDate}`);
+			expect(template('String: ${aString}', data)).to.equal(`String: ${data.aString}`);
+		});
+
+		it('should support handlebars format', ()=> {
+			expect(template('Hello {{name}}', {name: 'Jane'})).to.equal('Hello Jane');
+			expect(template('Baz is {{foo.bar.baz}}', {foo: {bar: {baz: 123}}})).to.equal('Baz is 123');
+			expect(template('Baz is {{foo.0.bar.0.baz.0}}', {foo: [{bar: [{baz: [123]}]}]})).to.equal('Baz is 123');
+			expect(template('Baz is {{0.bar.0.baz.0}}', [{bar: [{baz: [123]}]}])).to.equal('Baz is 123');
+			expect(template('Array traverse: ${1.2.3}', [0, [0, 0, [0, 0, 0, [123]]]])).to.equal('Array traverse: 123');
+		});
+
+		it('should render inline JavaScript', ()=> {
+			expect(template('Random ${Math.floor(100 * Math.random())}%')).to.match(/^Random (\d+)%$/);
+			expect(template('Epoch time is ${new Date().getTime()}')).to.match(/^Epoch time is (\d+)$/);
+			expect(template('Epoch time is {{new Date().getTime()}}')).to.match(/^Epoch time is (\d+)$/);
+		});
+
+		it('should handle null or undefined data access', ()=> {
+			let data = {quz: null, flarp: undefined};
+			expect(template('Foo: ${foo}', data)).to.equal(`Foo: undefined`);
+			expect(template("Bar: ${bar || 'Bar!'}", data)).to.equal(`Bar: Bar!`);
+			expect(template("Baz: ${foo || bar || baz || 'Baz!'}", data)).to.equal(`Baz: Baz!`);
+			expect(template("Quz: ${quz || 'Quz!'}", data)).to.equal(`Quz: Quz!`);
+			expect(template("Flarp: ${flarp || 'Flarp!'}", data)).to.equal(`Flarp: Flarp!`);
+			expect(()=> template("Kludge: ${kludge}", data, {safeUndefined: false})).to.throw;
+		});
+
 	});
 
-	it('should support handlebars format', ()=> {
-		expect(template('Hello {{name}}', {name: 'Jane'})).to.equal('Hello Jane');
-		expect(template('Baz is {{foo.bar.baz}}', {foo: {bar: {baz: 123}}})).to.equal('Baz is 123');
-		expect(template('Baz is {{foo.0.bar.0.baz.0}}', {foo: [{bar: [{baz: [123]}]}]})).to.equal('Baz is 123');
-		expect(template('Baz is {{0.bar.0.baz.0}}', [{bar: [{baz: [123]}]}])).to.equal('Baz is 123');
-		expect(template('Array traverse: ${1.2.3}', [0, [0, 0, [0, 0, 0, [123]]]])).to.equal('Array traverse: 123');
+	describe(`${method.toUpperCase()}: template.compile()`, ()=> {
+
+		it('should compile and run a template', ()=> {
+			let compiled = template.compile('Hello ${name}');
+			expect(compiled({name: 'Matt'})).to.equal('Hello Matt');
+			expect(compiled({name: 'Joe'})).to.equal('Hello Joe');
+		});
+
 	});
-
-	it('should render inline JavaScript', ()=> {
-		expect(template('Random ${Math.floor(100 * Math.random())}%')).to.match(/^Random (\d+)%$/);
-		expect(template('Epoch time is ${new Date().getTime()}')).to.match(/^Epoch time is (\d+)$/);
-		expect(template('Epoch time is {{new Date().getTime()}}')).to.match(/^Epoch time is (\d+)$/);
-	});
-
-	it('should handle null or undefined data access', ()=> {
-		var data = {quz: null, flarp: undefined};
-		expect(template('Foo: ${foo}', data)).to.equal(`Foo: undefined`);
-		expect(template("Bar: ${bar || 'Bar!'}", data)).to.equal(`Bar: Bar!`);
-		expect(template("Baz: ${foo || bar || baz || 'Baz!'}", data)).to.equal(`Baz: Baz!`);
-		expect(template("Quz: ${quz || 'Quz!'}", data)).to.equal(`Quz: Quz!`);
-		expect(template("Flarp: ${flarp || 'Flarp!'}", data)).to.equal(`Flarp: Flarp!`);
-		expect(()=> template("Kludge: ${kludge}", data, {safeUndefined: false})).to.throw;
-	});
-
-});
-
-describe('template.compile()', ()=> {
-
-	it('should compile and run a template', ()=> {
-		var compiled = template.compile('Hello ${name}');
-		expect(compiled({name: 'Matt'})).to.equal('Hello Matt');
-		expect(compiled({name: 'Joe'})).to.equal('Hello Joe');
-	});
-
 });
